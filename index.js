d2cbd01a3a0e1b22503e6abbac7d4765c60bf2fb
ChocoDream <dh99@live.se>
2021-09-30 18:48:33 +0200

    Animating colorchange on background, text and button color.

--- index.js
+++ index.js
@@ -12,11 +12,31 @@ const randomize = (array) => {
   return array[random(array.length)];
 };
 
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
 const renderInteractives = (quote) => {
   const { text: currentText } = quote;
   const currentAuthor = quote.author ?? "Unknown";
-  $("#text").text(`"${currentText}"`);
-  $("#author").text(`- ${currentAuthor}`);
+  const r = clamp(random(255), 25, 195);
+  const g = clamp(random(255), 25, 195);
+  const b = clamp(random(255), 25, 195);
+  $("#text").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $(this).text(currentText);
+  });
+  $("#author").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $(this).text(currentAuthor);
+  });
+  const currentColor = `rgb(${r}, ${g}, ${b})`;
+  $("html body").animate(
+    { backgroundColor: currentColor, color: currentColor },
+    1000
+  );
+  $(".btn").animate(
+    { backgroundColor: currentColor },
+    1000
+  );
 };
 
 $(document).ready(() => {
